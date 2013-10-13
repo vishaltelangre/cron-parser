a4d1b7c771e790bada92399518836d6268c513d3
Vishal Telangre <the@vishaltelangre.com>
2013-10-14 00:18:22 +0530

    prepare test environment

--- spec/cron/parser_spec.rb
+++ spec/cron/parser_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Cron::Parser do
-  # TODO
+  let(:parser_klass) { Cron::Parser }
+
+  context "Vroom..." do
+    it "check invalid pattern" do
+      expect { parser_klass.new }.to raise_error(InvalidCronPatternError, "cron pattern must be a string, please read documentation")
+      expect { parser_klass.new("") }.to raise_error(InvalidCronPatternError, "cron pattern must contain exact 5 fields seperated by whitespaces")
+    end
+  end
+
+  context "Check for individual fields" do
+    context "blah" do
+    end
+  end
 end
